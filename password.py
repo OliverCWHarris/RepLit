86dbcd22973d8e06f83e0fe31e18a7a9d5772a81
Oliver Harris <olivercwharris@gmail.com>
2021-09-21 14:45:14 +0100

    made it neater

--- password.py
+++ password.py
@@ -2,15 +2,22 @@
 user_name=str(input('enter a username: '))
 password=str(input('enter a password: '))
 
+
+
 #asks if you want to keep what you entred, displayes what you entred
 save=str(input('save credentials? (y/n)' + 'username: ' + user_name + ' password: ' + password + ': '))
+
+
 
 #decision making for what happens if you save credentials
 if save == 'y':
     #ask you to reenter your credentials
     print('verify credentials:')
+
     user_name_check=str(input('enter username: '))
+
     password_check=str(input('enter password: '))
+
 
     #if the username and password you entred matches what you entred earier it is saved
     #else it errors out and exits
