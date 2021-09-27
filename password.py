de3aa1f2ed5554cab0d959b5add7c8149a4ac3cd
Oliver Harris <olivercwharris@gmail.com>
2021-09-27 14:51:26 +0100

    fixed create accound, added non-function login

--- password.py
+++ password.py
@@ -17,46 +17,68 @@ def create():
     save=str(input('save credentials? (y/n): '))
 
     if save == 'y':
-        save()
+        #ask you to reenter your credentials
+        print('verify credentials')
+
+        user_name_check=str(input('enter username: '))
+
+        password_check=str(input('enter password: '))
+
+
+        #if the username and password you entred matches what you entred earier it is saved
+        #else it errors out and exits
+        if user_name_check == user_name and password_check == password:
+            print('correct credentials, saved loggin')
+
+            write_username=user_name+'\n'
+            write_password=password+'\n'
+            usernames = open("usernames.txt","w")
+            usernames.writelines(write_username)
+            usernames.close()
+
+            passwords = open("passwords.txt","w")
+            passwords.writelines(write_password)
+            passwords.close()
+
+        else:
+            print('invalid credentials, aborting...')
+            run=0
+            quit()
     
     #however if you dont save it, the program will just exit without saving
     elif save == 'n':
         print('credentials not saved, exiting...')
+        run=0
         quit()
     else:
         print('syntax error, exiting...')
 
 
-    #ask you to reenter your credentials
-    print('verify credentials')
-
-    user_name_check=str(input('enter username: '))
-
-    password_check=str(input('enter password: '))
-
-
-    #if the username and password you entred matches what you entred earier it is saved
-    #else it errors out and exits
-    if user_name_check == user_name and password_check == password:
-        print('correct credentials, saved loggin')
-
-        write_username=user_name+'\n'
-        write_password=password+'\n'
-        usernames = open("usernames.txt","w")
-        usernames.writelines(write_username)
-        usernames.close()
-
-        passwords = open("passwords.txt","w")
-        passwords.writelines(write_password)
-        passwords.close()
-
-    else:
-        print('invalid credentials, aborting...')
-        quit()
+    
 
 
 def login():
-    '2'
+    print("enter credentials:")
+    global login_check_u
+    login_check_u=str(input("enter username: "))
+    global login_check_p
+    login_check_p=str(input("enter pasword: "))
+
+    with open('usernames.txt') as f:
+        if login_check_u in f:
+            login_check_u=True
+    with open('passwords.txt') as f:
+        if login_check_p in f:
+            login_check_p=True
+    if login_check_u==True and login_check_p==True:
+        print("login successful!")
+        run=0
+        quit
+    else:
+        print("invalid credentials, exiting...")
+        run=0
+        quit
+
 
 
 
@@ -91,8 +113,7 @@ def menu():
 
 
 
-
+global run
 run=1
 if run == 1:
     menu()
-    run=0
