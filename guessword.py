7362af52426a0f864ce116f737434b0ece94f833
Oliver Harris <olivercwharris@gmail.com>
2021-11-15 13:45:39 +0000

    yeah it pretty much works

--- guessword.py
+++ guessword.py
@@ -7,10 +7,10 @@ import os
 def main():
     global guesses
     global word
+    global word_length
     word=str(input("p3 enter word\n: "))
     word_length=len(word)
     os.system('cls')
-    print(word_length)
     guesses1()
 
 
@@ -18,10 +18,23 @@ def guesses1():
     global guesses
     global number
 
-    guess=str(input("p1 enter first guess\n: "))
+    print("letters in word: ",word_length)
+    guess=str(input("p1 enter guess\n: "))
     if guess in word:
-        print("correct! ",guess," is in the word")
+        word_count=word.count(guess)
+        print("correct! ",guess," is in the word, ",word_count," times!")
+
+        letter_position=word.index(guess)
+        print("The letter: ",guess," is letter: ",letter_position+1)
+        
+        if word_count>=2:
+            print("and in other places!")
+
         guesses2()
+
+    elif guess==word:
+        print("that is the word! well done!\nPlayer 1 wins!")
+        quit
 
     else:
         print("sorry, ",guess," is not in the word")
@@ -31,10 +44,24 @@ def guesses1():
 def guesses2():
     global guesses
     global number
-    guess=str(input("p2 enter first guess\n: "))
+
+    print("letters in word: ",word_length)
+    guess=str(input("p2 enter guess\n: "))
     if guess in word:
-        print("correct! ",guess," is in the word")
+        word_count=word.count(guess)
+        print("correct! ",guess," is in the word, ",word_count," times!")
+
+        letter_position=word.index(guess)
+        print("The letter: ",guess," is letter: ",letter_position+1)
+        
+        if word_count>=2:
+            print("and in other places!")
+        
         guesses1()
+
+    elif guess==word:
+        print("that is the word! well done!\nPlayer 2 wins!")
+        quit
 
     else:
         print("sorry, ",guess," is not in the word")
