19959958c7b820359402f09a3f31543d213f888d
Oliver Harris <olivercwharris@gmail.com>
2021-11-16 18:32:01 +0000

    fixed everything, all working

--- guessword.py
+++ guessword.py
@@ -20,22 +20,15 @@ def guesses1():
 
     print("letters in word: ",word_length)
     guess=str(input("p1 enter guess\n: "))
-    if guess in word:
-        word_count=word.count(guess)
-        print("correct! ",guess," is in the word, ",word_count," times!")
-
-        letter_position=word.index(guess)
-        print("The letter: ",guess," is letter: ",letter_position+1)
-        
-        if word_count>=2:
-            print("and in other places!")
-
-        guesses2()
-
-    elif guess==word:
+    if guess==word:
         print("that is the word! well done!\nPlayer 1 wins!")
         quit
 
+    elif guess in word:
+        letter_position=word.index(guess)
+        print("The letter: ",guess," is letter: ",letter_position+1)
+
+        guesses2()
     else:
         print("sorry, ",guess," is not in the word")
         guesses2()
@@ -47,21 +40,16 @@ def guesses2():
 
     print("letters in word: ",word_length)
     guess=str(input("p2 enter guess\n: "))
-    if guess in word:
-        word_count=word.count(guess)
-        print("correct! ",guess," is in the word, ",word_count," times!")
 
+    if guess==word:
+        print("that is the word! well done!\nPlayer 2 wins!")
+        quit
+
+    elif guess in word:
         letter_position=word.index(guess)
         print("The letter: ",guess," is letter: ",letter_position+1)
         
-        if word_count>=2:
-            print("and in other places!")
-        
         guesses1()
-
-    elif guess==word:
-        print("that is the word! well done!\nPlayer 2 wins!")
-        quit
 
     else:
         print("sorry, ",guess," is not in the word")
